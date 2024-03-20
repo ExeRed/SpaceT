85b4f2b861ff42b0a1a3911051f4580033f7b612
Valentyn Bibik <94911912+ExeRed@users.noreply.github.com>
2024-03-20 09:58:05 +0100

    Add files via upload

--- src/main/resources/static/script/burger_menu.js
+++ src/main/resources/static/script/burger_menu.js
@@ -17,3 +17,14 @@ function closeMenu() {
     navMenu.classList.remove("active");
 }
 
+function changeTextSize(action) {
+    var currentSize = parseInt($("html").css("font-size"));
+
+    if (action === 'increaseBy4') {
+        $("html").css("font-size", currentSize + 4 + "px");
+    } else if (action === 'increaseBy2') {
+        $("html").css("font-size", currentSize + 2 + "px");
+    } else if (action === 'reset') {
+        $("html").css("font-size", "10px");
+    }
+}
