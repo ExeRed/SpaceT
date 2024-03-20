7e0eaa0e05c49852fbc9b451c7068724a78bbdd4
Valentyn Bibik <94911912+ExeRed@users.noreply.github.com>
2024-03-20 10:01:41 +0100

    Delete src directory

--- src/main/java/com/example/SpringT/repo/EmailRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.SpringT.repo;
-
-import com.example.SpringT.models.Email;
-import com.example.SpringT.models.Ticket;
-import org.springframework.data.repository.CrudRepository;
-
-public interface EmailRepository extends CrudRepository<Email, Long> {
-}
--- src/main/java/com/example/SpringT/repo/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.SpringT.repo;
-
-import com.example.SpringT.models.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-}
